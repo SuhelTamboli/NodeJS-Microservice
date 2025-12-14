cba98591f1e973ebacbac3d11928e9c0ae0148bf
Suhel Tamboli <suhelmtamboli9@gmail.com>
2025-12-14 16:43:23 +0530

    added connection request review api

--- src/routes/connectionRequestRouter.js
+++ src/routes/connectionRequestRouter.js
@@ -15,7 +15,7 @@ const ConnectionRequest = require("../models/ConnectionRequest");
 connectionRequestRouter.post(
   "/request/send/:status/:userId",
   isAuthorized,
-  //sanitize request data => only accepted status will be interested/ignored
+  //sanitize request data => only allowed statuses will be interested/ignored
   blockIncorrectStatus(["interested", "ignored"]),
   validateReceiverId,
   async (req, res) => {
@@ -38,7 +38,7 @@ connectionRequestRouter.post(
       });
     } catch (error) {
       console.error(error.message);
-      //send response in case login API fails
+      //send response in case sending connection request API fails
       res.status(400).json({
         msg: "Error while sending connection request",
         error: error.message,
@@ -52,5 +52,56 @@ connectionRequestRouter.post(
 //send receivers userId in request
 //we can get senders userId from auth middleware
 // POST /request/review/:status/:requestId
+connectionRequestRouter.patch(
+  "/request/review/:status/:requestId",
+  isAuthorized,
+  //sanitize request data => only allowed statuses will be accepted/rejected
+  blockIncorrectStatus(["accepted", "rejected"]),
+  async (req, res, next) => {
+    try {
+      const { status, requestId } = req.params;
+      const { user: loggedInUser } = req;
+
+      //the requestId should only belong to connectionRequest with status as interested
+      const connectionRequest = await ConnectionRequest.findById(requestId);
+
+      //if connection request is not in DB
+      if (!connectionRequest) {
+        return res.status(400).json({
+          msg: "Connection request not found",
+          error: null,
+          data: null,
+        });
+      }
+
+      //loggedIn user should be toUser for this request
+      console.log(connectionRequest);
+      if (!loggedInUser._id.equals(connectionRequest.toUserId)) {
+        return res.status(400).json({
+          msg: "This connection request does not belong to you",
+          error: null,
+          data: null,
+        });
+      }
+
+      //update status and save updated connection request
+      connectionRequest.status = status;
+      const updatedConnectionRequest = await connectionRequest.save();
+      res.json({
+        msg: "Connection request updated successfully",
+        error: null,
+        data: updatedConnectionRequest,
+      });
+    } catch (error) {
+      console.error(error.message);
+      //send response in case reviewing connection request API fails
+      res.status(400).json({
+        msg: "Error while reviewing connection request",
+        error: error.message,
+        data: null,
+      });
+    }
+  }
+);
 
 module.exports = connectionRequestRouter;
